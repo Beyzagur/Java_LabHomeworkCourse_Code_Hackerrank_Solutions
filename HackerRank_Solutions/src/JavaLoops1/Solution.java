55ad41f69697938a03d382f8b0de8f9ee13661a3
Beyzagur <58330314+Beyzagur@users.noreply.github.com>
2020-11-03 20:46:20 +0300

    My Java Loops 1 solution

---
+++ HackerRank_Solutions/src/JavaLoops1/Solution.java
@@ -0,0 +1,22 @@
+package JavaLoops1;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		int result = 0;
+
+		if (2 <= n && n <= 20) {
+
+			for (int i = 1; i < 11; i++) {
+				result = n * i;
+				System.out.println(n + " x " + i + " = " + result);
+			}
+		}
+
+	}
+}
