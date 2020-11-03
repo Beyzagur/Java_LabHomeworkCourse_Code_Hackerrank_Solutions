f36b74419bc603d07bd702f184a6dba825c71947
Beyzagur <58330314+Beyzagur@users.noreply.github.com>
2020-11-03 19:04:55 +0300

    My java stdin and stdout 1 solution

---
+++ HackerRank_Solutions/src/JavaStdinAndStdout1/Solution.java
@@ -0,0 +1,21 @@
+package JavaStdinAndStdout1;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    	
+        Scanner scan = new Scanner(System.in);
+        
+        int myFirstNumber = scan.nextInt();
+        int mySecondNumber = scan.nextInt();
+        int myThirdNumber = scan.nextInt();
+        
+
+        System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
+        
+    }
+}
