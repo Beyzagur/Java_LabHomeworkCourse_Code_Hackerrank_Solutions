cbed0845e3ec33c1359c1995190b23e7b6706d8f
Beyzagur <58330314+Beyzagur@users.noreply.github.com>
2020-11-11 01:12:20 +0300

    Lab1 part2:Inheritance

--- DataStructures_And_Algorithms/src/Inheritance/Test.java
+++ DataStructures_And_Algorithms/src/Inheritance/Test.java
@@ -3,15 +3,15 @@ package Inheritance;
 public class Test {
 
 	public static void main(String[] args) {
-		Point p=new Point(0,0);
-		Circle c=new Circle(2,4);
-		Circle c1=new Circle();
-		
+		Point p = new Point(0, 0);
+		Circle c = new Circle(2, 4);
+		Circle c1 = new Circle();
+
 		System.out.println();
 		p.Display();
 		c.Display();
 		System.out.println();
-		System.out.println(c.Calculate(new Circle(9,5)));
+		System.out.println(c.Calculate(new Circle(9, 5)));
 	}
 
 }
